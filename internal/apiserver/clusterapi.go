63d251b2414d139daca04ff2ed8ba455f1f13441
David E Watson <davidewatson@gmail.com>
2018-12-04 11:45:08 -0800

    Revert "Add labels to Machine manifests so that the Cluster API can apply"

--- internal/apiserver/clusterapi.go
+++ internal/apiserver/clusterapi.go
@@ -41,17 +41,6 @@ type SSHMachineParams struct {
 	Host     string
 	Port     int32
 	Password string
-	Labels   map[string]string
-}
-
-func translateKubernetesLabels(in []*pb.KubernetesLabel) map[string]string {
-	labels := make(map[string]string)
-
-	for _, l := range in {
-		labels[l.Name] = l.Value
-	}
-
-	return labels
 }
 
 func TranslateCreateClusterMsg(in *pb.CreateClusterMsg) SSHClusterParams {
@@ -67,7 +56,6 @@ func TranslateCreateClusterMsg(in *pb.CreateClusterMsg) SSHClusterParams {
 			Password: m.Password,
 			Host:     m.Host,
 			Port:     m.Port,
-			Labels:   translateKubernetesLabels(m.Labels),
 		})
 	}
 
@@ -77,7 +65,6 @@ func TranslateCreateClusterMsg(in *pb.CreateClusterMsg) SSHClusterParams {
 			Password: m.Password,
 			Host:     m.Host,
 			Port:     m.Port,
-			Labels:   translateKubernetesLabels(m.Labels),
 		})
 	}
 
@@ -98,7 +85,6 @@ func TranslateAdjustClusterMsg(in *pb.AdjustClusterMsg, version, publicKey, priv
 			Password: m.Password,
 			Host:     m.Host,
 			Port:     m.Port,
-			Labels:   translateKubernetesLabels(m.Labels),
 		})
 	}
 
